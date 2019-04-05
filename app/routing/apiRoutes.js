e55f95715e961930dc44d9c77c2c45657d377544
Ky-Ann Long <kyannlong@gmail.com>
2019-04-04 21:32:49 -0400

    final js and logic changes made

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,37 +1,34 @@
 //route links to data source
 var friends = require("../data/friends");
-// var yournewFriend = require("../data/newFriend");
+ 
+// console.log(friends);
 
-console.log(friends);
 module.exports = function(app) {
-//API GET Request
+  //API GET Request 
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
 
-    app.get("/api/friends", function(req, res) {
-        res.json(friends);
-      });
+  // console.log(friends);
 
-      console.log(friends);
+  app.post("/api/friends", function(req, res) {
+    let newfriend = req.body;
+    let totaldifference = 99;
+    let match;
+    //algorhythm here
+    for (i =0; i < friends.length; i++) {
+      let newTotalDiff = 0;
+      for (j = 0; j < friends[i].scores.length; j++) {
+        newTotalDiff += Math.abs(newfriend.scores[j] - friends[i].scores[j]);
+      } 
+      //check to see if their scores are clsoer than current friends scores
+      if (newTotalDiff < totaldifference) {
+        match = friends[i];
+        totaldifference = newTotalDiff;
+      }
+    }
 
-      app.post("/api/friends", function(req, res) {
-        // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-        // It will do this by sending out the value "true" have a table
-        // req.body is available since we're using the body parsing middleware
-        
-        console.log('IN THE POST!!');
-        console.log(req.body);
-        for (var i = 0; i < friends.length; i++) {
-            friends.push(req.body);
-            res.json(false);
-        }
-        
-        
-        // if (surveyData.length < 5) {
-        //   surveyData.push(req.body);
-        //   res.json(true);
-        // }
-        // else {
-        //   yournewFriend.push(req.body);
-        //   res.json(false);
-        // }
-      });
-}
+    console.log('match: %o', match);
+    res.json(match);
+  });
+};
---
+++ scratch.js
@@ -0,0 +1,12 @@
+var yournewFriend = {name: "",
+photo: "",
+scores: ""};
+
+
+
+var array1 = [5, 1, 4, 4, 5, 1, 2, 5, 4, 1];
+var array2 = [3, 2, 6, 4, 5, 1, 2, 5, 4, 1];
+
+for (var i = 0; i < userdata.length; i++); {
+ console.log(userdata[i]);
+}
